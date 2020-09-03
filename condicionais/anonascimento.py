33a4a05331a6079c00358f07a601dba51780c688
ana <gomesabam@gmail.com>
2020-09-03 11:34:18 -0300

    Data de aniversario correta

--- condicionais/anonascimento.py
+++ condicionais/anonascimento.py
@@ -1,5 +1,12 @@
-def sua_idade(atual, nasc):
-    return atual - nasc
+def sua_idade(atual, nasc, mes_today, mes_born, today, born):
+    bth = atual - nasc
+    #se o mes atual é maior do seu aniversario
+    if  mes_today < mes_born:
+        bth = bth -1
+    elif mes_today == mes_born:
+        if today < born:
+            bth = bth -1
+    return bth
 
 
 def main():
@@ -11,9 +18,15 @@ def main():
     mes2 = int(input("Mês de seu nascimento: "))
     ano2 = int(input("Ano em que nasceu: "))
 
-    idade = sua_idade(ano1, ano2)
+    idade = sua_idade(ano1, ano2, mes1, mes2, dia1, dia2)
+    
+    if dia1 == dia2:
+        print(f'Hoje {dia1}/{mes1}/{ano1} é seu {idade} aniversario, parabéns!')
+    elif dia1 < dia2:
+        print(f'Ainda não é seu aniversario')
+    elif dia1 > dia2:
+        print(f'Seu aniversário já passou!')
 
-    print(f'Hoje {dia1}/{mes1}/{ano1} é seu {idade} aniversario, parabéns!')
 
 if __name__=='__main__':
     main()
