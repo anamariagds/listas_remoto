7ad6ff4a1ef2b075f61627766c606c627ed1869d
ana <gomesabam@gmail.com>
2020-09-12 01:01:04 -0300

    meu nome com turtle

--- projetos_pec166/minhatartaruga.py
+++ projetos_pec166/minhatartaruga.py
@@ -1,11 +1,63 @@
-from turtle import 
+from turtle import * 
 
 shape("turtle")
 speed(11)
 pensize(6)
 color("#B22222")
 
-goto(1,6)
+#letraA
+left(60)
+forward(150)
+
+right(120)
+forward(150)
+
+backward(60)
+right(120)
+forward(90)
+
+penup()
+backward(90)
+left(120)
+forward(60)
+
+left(60)
+forward(20)
+
+color("#008000")
+pendown()
+
+#letraN
+left(90)
+forward(150)
+right(150)
+forward(160)
+
+left(150)
+forward(150)
+
+#pausacaneta
+penup()
+backward(20)
+right(90)
+forward(100)
+pendown()
+
+color("blue")
+#letraA2
+right(120)
+forward(150)
+
+backward(150)
+left(60)
+forward(150)
+backward(60)
+right(120)
+forward(90)
+
+
+
+
 
 
 done()
