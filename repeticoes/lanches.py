ec4f6d1df2cbfe7d5d25cc50c21d65324a5964a4
ana <gomesabam@gmail.com>
2020-09-19 22:31:12 -0300

    origin master

--- repeticoes/lanches.py
+++ repeticoes/lanches.py
@@ -1,34 +1,35 @@
 def lanche():
     conta = 0
     while True:
-        codigo = input().upper()[0]
+        codigo = input('''
+CÓDIGO  PRODUTO         PREÇO (R$)
+H       Hamburger       5,50
+C       Cheeseburger    6,80
+M       Misto Quente    4,50
+A       Americano       7,00
+Q       Queijo Prato    4,00
+X       PARA TOTAL DA CONTA''').upper()[0]      
         if codigo == 'H':
-            conta += 5.50
+            conta += 5.50           
         elif codigo == 'C':
-            conta +=6.80
+            conta += 6.80
         elif codigo == 'M':
-            conta+= 4.50
-        elif codigo == 'A':
+            conta+=4.50
+        elif codigo == 'A':       
             conta +=7.00
         elif codigo == 'Q':
             conta += 4.00
-        if codigo == 'X':
-            print(f'{conta:.2f}')
-            break
+
         if codigo != 'H' and codigo != 'C' and codigo != 'M' and codigo != 'A' and codigo != 'Q' and codigo != 'X':
             print("Opção inválida.")
 
+        if codigo == 'X':
+            return conta
+            break
+
 def main():
-    print('''
-CÓDIGO  PRODUTO          PREÇO (R$)
-H       Hamburger        5.50
-C       Cheeseburger     6.80
-M       Misto Quente     4.50
-A       Americano        7.00
-Q       Queijo Prato     4.00
-X       PARA TOTAL DA CONTA
-''')
-    lanche()
+    pagar = lanche()
+    print(f'\nSua conta deu R${pagar:.2f}')
 
 
 if __name__=='__main__':
