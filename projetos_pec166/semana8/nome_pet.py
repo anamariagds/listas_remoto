4253c4596ef24b7c6eec1df41c69f44d29e2af42
ana <gomesabam@gmail.com>
2020-10-01 23:40:30 -0300

    nome de pet

--- projetos_pec166/semana8/nome_pet.py
+++ projetos_pec166/semana8/nome_pet.py
@@ -1,61 +1,62 @@
 #programa que escolhe nome para pet feme ou macho
 from random import *
+def selecionanome():
+    executa = True
 
-executa = True
+    nomefemea = ['Dora', 'Amora', "Belinha", 'Mel']
+    nomemacho = ['Rafael', 'Dante', 'Bilie', 'Bob']
+    print("Escolha de nome para animal")
 
-nomefemea = ['Dora', 'Amora', "Belinha", 'Mel']
-nomemacho = ['Rafael', 'Dante', 'Bilie', 'Bob']
+    print("xxxxxxxxxxxxxxxxxxxxxxx")
+    print('''
+    Menu:
+        a - Ver o nome
+        b - Ver listas
+        c - Adicionar nomes na lista
+        s - Sair
+    ''')
 
-print("Escolha de nome para animal")
-
-print("xxxxxxxxxxxxxxxxxxxxxxx")
-
-print('''
-Menu:
-    a - Ver o nome
-    b - Ver listas
-    c - Adicionar nomes na lista
-    s - Sair
-
-
-''')
-while executa == True:
-    menu= input("\n>_").lower()[0]
+    while executa == True:
+        menu= input("\n>_").lower()[0]
     #escolhe nome na lista atual
-    if menu == 'a':
-        n = int(input("Quantos nomes você precisa?"))
-        for i in range(n):
-            print("m se macho, f se femea: ")
-            eh = input()[0]
-            if eh.lower() == 'f':
-                print("O nome para seu pet é:", choice(nomefemea))
-            elif eh.lower() =='m':
-                print("O nome para seu pet é:", choice(nomemacho))
+        if menu == 'a':
+            n = int(input("Quantos nomes você precisa?"))
+            for i in range(n):
+                print("f se femea, m- se macho: ")
+                eh = input()[0]
+                if eh.lower() == 'f':
+                    print("O nome para seu pet é:", choice(nomefemea))
+                elif eh.lower() =='m':
+                    print("O nome para seu pet é:", choice(nomemacho))
     # Ver lista:
-    elif menu =='b':
-        print(nomefemea)
-        print(nomemacho)
+        elif menu =='b':
+            print(nomefemea)
+            print(nomemacho)
     #adicionar nome na lista
-    elif menu =='c':
-        ql = int(input("1- para nome de femea. 2- nome de pet macho"))
-        if ql == 1:
-            nomeAdd = input("Adicione o nome: ")
-            if nomeAdd in nomefemea:
-                print("Esse nome já está na lista!")
-            else:
-                nomefemea.append(nomeAdd)
+        elif menu =='c':
+            ql = int(input("1- para nome de femea. 2- nome de pet macho"))
+            if ql == 1:
+                nomeAdd = input("Adicione o nome: ")
+                if nomeAdd in nomefemea:
+                    print("Esse nome já está na lista!")
+                else:
+                    nomefemea.append(nomeAdd)
         
-        if ql == 2:
-            nomeAdd = input("Adicione o nome: ")
-            if nomeAdd in nomemacho:
-                print("Esse nome já está na lista!")
-            else:
-                nomemacho.append(nomeAdd)
+            if ql == 2:
+                nomeAdd = input("Adicione o nome: ")
+                if nomeAdd in nomemacho:
+                    print("Esse nome já está na lista!")
+                else:
+                    nomemacho.append(nomeAdd)
     #finalizar programa
-    elif menu =='s':
-        executa = False
-    else:
-        print("Insira opção válida!")
+        elif menu =='s':
+            executa = False
+        else:
+            print("Insira opção válida!")
+def main():
+    selecionanome()
+    
 
-
+if __name__=='__main__':
+    main()
 
